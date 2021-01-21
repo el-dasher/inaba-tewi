a65987c5c3e8ebb22792f34b6d5599faa419e01a
el-dasher <74831622+el-dasher@users.noreply.github.com>
2021-01-21 18:36:32 -0400

    pip freezed requirements.txt

--- config/firebase/database.py
+++ config/firebase/database.py
@@ -1,11 +1,10 @@
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin.credentials import Certificate
-from google.cloud import firestore_v1
 
 from .firebase_sdk_config import FIREBASE_CONFIG
 
 firebase_credential: Certificate = Certificate(FIREBASE_CONFIG)
 
 TEWI_FIREBASE_APP: firebase_admin.App = firebase_admin.initialize_app(firebase_credential)
-TEWI_DB: firestore_v1.client.Client = firestore.client(TEWI_FIREBASE_APP)
+TEWI_DB = firestore.client(TEWI_FIREBASE_APP)
