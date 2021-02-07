302ca67a09e81217f91cf37a0a4dcea5c36e316b
el-dasher <74831622+el-dasher@users.noreply.github.com>
2021-02-07 17:28:32 -0400

    update bot_setup.py

---
+++ cogs/osu/droid/tasks/cogs/inaba-tewi/utils/bot_setup.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+
+from utils.env_setup import setup_env
+
+# DEBUG is here so some behaviours are changed while debugging
+DEBUG: bool = False
+setup_env()
+
+DEFAULT_BOT_PREFIXES: Tuple[str, ...]
+if DEBUG:
+    DEFAULT_BOT_PREFIXES = ('debug', 'owo!', 'd!')
+else:
+    DEFAULT_BOT_PREFIXES = ('tewi!', 'uwu!', 'i!')
+
+_RAW_COGS: Tuple[str, ...] = (
+    'osu.droid.userbind', 'osu.droid.profile', 'osu.droid.recent', 'osu.droid.compare',
+    'osu.droid.submit', 'osu.droid.ppcheck',
+    'osu.ppy.beatmap_calc', 'osu.ppy.beatmap_search',
+    'osu.droid.tasks.br_rank',
+    'handlers.error_handler',
+    'tools.avatar'
+)
+
+COGS: tuple = tuple(map(lambda cog_path: f'cogs.{cog_path}', _RAW_COGS))
